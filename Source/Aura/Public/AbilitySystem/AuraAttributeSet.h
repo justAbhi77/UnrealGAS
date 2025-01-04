270f1dd593bef5d009d66b00c455c14d6a7c741f
AbhinavOjha <abhinav@elearningdesigncenter.com>
2025-01-04 14:46:22 +0530

    Unreal Macros are not compatible with cpp macros :(

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -7,25 +7,25 @@
 #include "AbilitySystemComponent.h"
 #include "AuraAttributeSet.generated.h"
 
-// Macro to generate standard accessor functions for attributes
+// Macro to generate standard accessor functions for attributes. Can be used as a macro
 #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
 	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
-// Macro to generate OnRep function for @PropertyName
-#define ON_REP_IMPL(PropertyName) \
-void OnRep_##PropertyName(const FGameplayAttributeData& OldValue) const { \
-GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, PropertyName, OldValue); \
-}
-
-// Macro to generate Attribute (let compiler do the heavy lifting :))
+/**
+ * Macro to generate Attribute (let compiler do the heavy lifting :)). Can not be used as a macro.
+ * Use this for copying the preview and delete $ sign.
+ */
 #define ATTRIBUTE_PROPERTY(PropertyName) \
-UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_##PropertyName, Category = "Attributes") \
-FGameplayAttributeData PropertyName; \
-ATTRIBUTE_ACCESSORS(ThisClass, PropertyName); \
-UFUNCTION() ON_REP_IMPL(PropertyName);
+$UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_##PropertyName, Category = "Attributes") \
+$FGameplayAttributeData  PropertyName; \
+$ATTRIBUTE_ACCESSORS(ThisClass, PropertyName); \
+$UFUNCTION()\
+void OnRep_##PropertyName(const FGameplayAttributeData& OldValue) const { \
+$GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, PropertyName, OldValue); \
+}
 
 
 // Struct to encapsulate effect-related properties
@@ -67,6 +67,7 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
 public:
 	UAuraAttributeSet();
 
@@ -80,26 +81,103 @@ public:
 	TMap<FGameplayTag, TStaticFuncPtr<FGameplayAttribute()>> TagsToAttributes;
 
 	// Primary Attributes
-	ATTRIBUTE_PROPERTY(Strength);
-	ATTRIBUTE_PROPERTY(Intelligence);
-	ATTRIBUTE_PROPERTY(Resilience);
-	ATTRIBUTE_PROPERTY(Vigor);
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Strength, Category = "Attributes")
+	FGameplayAttributeData Strength;
+	ATTRIBUTE_ACCESSORS(ThisClass, Strength);
+
+	UFUNCTION() void OnRep_Strength(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Strength, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Intelligence, Category = "Primary Attributes")
+	FGameplayAttributeData Intelligence;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Intelligence);
+
+	UFUNCTION() void OnRep_Intelligence(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Intelligence, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Resilience, Category = "Attributes")
+	FGameplayAttributeData Resilience;
+	ATTRIBUTE_ACCESSORS(ThisClass, Resilience);
+
+	UFUNCTION() void OnRep_Resilience(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Resilience, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Vigor, Category = "Primary Attributes")
+	FGameplayAttributeData Vigor;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Vigor);
+
+	UFUNCTION() void OnRep_Vigor(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Vigor, OldValue); }
 
 	// Secondary Attributes
-	ATTRIBUTE_PROPERTY(Armor);
-	ATTRIBUTE_PROPERTY(ArmorPenetration);
-	ATTRIBUTE_PROPERTY(BlockChance);
-	ATTRIBUTE_PROPERTY(CriticalHitChance);
-	ATTRIBUTE_PROPERTY(CriticalHitDamage);
-	ATTRIBUTE_PROPERTY(CriticalHitResistance);
-	ATTRIBUTE_PROPERTY(HealthRegeneration);
-	ATTRIBUTE_PROPERTY(ManaRegeneration);
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Armor, Category = "Attributes")
+	FGameplayAttributeData Armor;
+	ATTRIBUTE_ACCESSORS(ThisClass, Armor);
+
+	UFUNCTION() void OnRep_Armor(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Armor, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_ArmorPenetration, Category = "Attributes")
+	FGameplayAttributeData ArmorPenetration;
+	ATTRIBUTE_ACCESSORS(ThisClass, ArmorPenetration);
+
+	UFUNCTION() void OnRep_ArmorPenetration(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, ArmorPenetration, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_BlockChance, Category = "Attributes")
+	FGameplayAttributeData BlockChance;
+	ATTRIBUTE_ACCESSORS(ThisClass, BlockChance);
+
+	UFUNCTION() void OnRep_BlockChance(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, BlockChance, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_CriticalHitChance, Category = "Attributes")
+	FGameplayAttributeData CriticalHitChance;
+	ATTRIBUTE_ACCESSORS(ThisClass, CriticalHitChance);
+
+	UFUNCTION() void OnRep_CriticalHitChance(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, CriticalHitChance, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_CriticalHitDamage, Category = "Attributes")
+	FGameplayAttributeData CriticalHitDamage;
+	ATTRIBUTE_ACCESSORS(ThisClass, CriticalHitDamage);
+
+	UFUNCTION() void OnRep_CriticalHitDamage(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, CriticalHitDamage, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_CriticalHitResistance, Category = "Attributes")
+	FGameplayAttributeData CriticalHitResistance;
+	ATTRIBUTE_ACCESSORS(ThisClass, CriticalHitResistance);
+
+	UFUNCTION() void OnRep_CriticalHitResistance(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, CriticalHitResistance, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_HealthRegeneration, Category = "Attributes")
+	FGameplayAttributeData HealthRegeneration;
+	ATTRIBUTE_ACCESSORS(ThisClass, HealthRegeneration);
+
+	UFUNCTION() void OnRep_HealthRegeneration(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, HealthRegeneration, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_ManaRegeneration, Category = "Attributes")
+	FGameplayAttributeData ManaRegeneration;
+	ATTRIBUTE_ACCESSORS(ThisClass, ManaRegeneration);
+
+	UFUNCTION() void OnRep_ManaRegeneration(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, ManaRegeneration, OldValue); }
 
 	// Vital Attributes
-	ATTRIBUTE_PROPERTY(Health);
-	ATTRIBUTE_PROPERTY(Mana);
-	ATTRIBUTE_PROPERTY(MaxHealth);
-	ATTRIBUTE_PROPERTY(MaxMana);
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(ThisClass, Health);
+
+	UFUNCTION() void OnRep_Health(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Health, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(ThisClass, Mana);
+
+	UFUNCTION() void OnRep_Mana(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Mana, OldValue); }
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(ThisClass, MaxHealth);
+
+	UFUNCTION() void OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, MaxHealth, OldValue); }
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Attributes")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(ThisClass, MaxMana);
+
+	UFUNCTION() void OnRep_MaxMana(const FGameplayAttributeData& OldValue) const { GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, MaxMana, OldValue); }
 
 private:
 	void SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
